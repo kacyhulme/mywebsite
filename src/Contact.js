d589864db917e059c8b6bcc73bae4ef704be4c75
Kacy Hulme <kacyhulme@gmail.com>
2022-01-19 20:04:54 -0500

    Add input fields to contact form

--- src/Contact.js
+++ src/Contact.js
@@ -1,10 +1,38 @@
 import { Link } from "react-router-dom";
+import React, { useState } from 'react';
 
 function Contact() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: ''
+  });
+
+  const handleInput = (e) => {
+    setFormData(startingValues=> ({
+      ...startingValues,
+      [e.target.name]: e.target.value    
+    }));
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("form data",formData)
+  }
+
   return (
     <div>
       <Link to="/">Home</Link>
       <h2>Contact</h2>
+      <form>
+        <label>
+          Name:
+          <input name="name" type="text" value={formData.name} onChange={ handleInput }/>
+        </label>
+        <label>
+          Email:
+          <input name="email" type="email" value={formData.email} onChange={ handleInput }/>
+        </label>
+        <button type="submit" onClick={handleSubmit}>Send</button>
+      </form>
     </div>
   )
 }
