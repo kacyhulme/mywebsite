70afe17804de9ef0f699f7a8637abbc77d95cebe
Kacy Hulme <kacyhulme@gmail.com>
2022-01-27 13:11:47 -0500

    Add resume in plain HTML

--- src/Resume.js
+++ src/Resume.js
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
+import ResumeHTML from './resumehtml.js'
 
 function Resume() {
   return (
     <div>
       <Link to="/">Home</Link>
       <h2>Resume</h2>
+      <ResumeHTML/>
     </div>
   )
 }
