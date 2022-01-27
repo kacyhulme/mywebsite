9f9737fb8eafd90cd559832f5bbad067508607b0
Kacy Hulme <kacyhulme@gmail.com>
2022-01-27 13:20:19 -0500

    Upload headshot

--- src/About.js
+++ src/About.js
@@ -1,12 +1,20 @@
 import { Link } from "react-router-dom";
+import Headshot from './headshot.jpg';
+import styled from 'styled-components';
 
 function About() {
   return (
     <div>
       <Link to="/">Home</Link>
       <h2>About</h2>
+      <HeadshotStyles src={Headshot} className="" alt="headshot" />
     </div>
   )
 }
+
+const HeadshotStyles = styled.img`
+  width: auto;
+  width 10rem;
+`
 
 export default About;
