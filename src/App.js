240070bb2e3c369f4d0e1b82167b20e650dedd9f
Kacy Hulme <kacyhulme@gmail.com>
2022-01-27 21:05:53 -0500

    Add and try react spring library

--- src/App.js
+++ src/App.js
@@ -4,11 +4,23 @@ import Nav from './Nav.js';
 import ReactModal from 'react-modal';
 import React, { useState } from 'react';
 import Resume from './Resume.js';
+import { useSpring, animated } from 'react-spring';
+import { config } from 'react-spring';
 
 ReactModal.setAppElement('#root');
 
 function App() {
   const [modalIsOpen, setIsOpen] = useState(false);
+  const [flip, set] = useState(false);
+  const props = useSpring({
+    to: { opacity: 1 },
+    from: { opacity: 0 },
+    reset: true,
+    reverse: flip,
+    delay: 200,
+    config: config.molasses,
+    onRest: () => set(!flip),
+  })
 
   function handleOpenModal() {
     setIsOpen(true);
@@ -25,6 +37,7 @@ function App() {
         <p>
           Kacy Hulme
         </p>
+        <animated.h1 style={props}>hello</animated.h1>
         <button onClick={handleOpenModal}>Click to See Resume</button>
         <p>
           Front End Web Developer
