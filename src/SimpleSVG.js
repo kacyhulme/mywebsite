a68fc8a19b5cd8dc142ec72d117dfc1ce7fa12f4
Kacy Hulme <kacyhulme@gmail.com>
2022-02-05 08:32:35 -0500

    Update points and viewBox for simple SVG

--- src/SimpleSVG.js
+++ src/SimpleSVG.js
@@ -1,12 +1,54 @@
 function SimpleSVG() {
   
   return (
-    <svg height="80" width="80" viewBox="0 0 45 45">
+    <svg height="180" width="180" viewBox="0 0 125 125">
       <polygon 
-      points="22.5 35.25 8.68704657 42.5118994 11.3250859 27.1309497 0.150171867 16.2381006 15.5935233 13.9940503 22.5 0 29.4064767 13.9940503 44.8498281 16.2381006 33.6749141 27.1309497 36.3129534 42.5118994" 
+      points="45,23,51.4656377752172,36.10081306187558,65.92324335849338,38.201626123751154,55.46162167924669,48.39918693812442,57.931275550434414,62.798373876248846,45,56,32.06872444956559,62.798373876248846,34.53837832075331,48.39918693812442,24.07675664150662,38.20162612375116,38.5343622247828,36.10081306187558" 
       style={{fill:'purple',stroke:'red',strokeWidth:1}} />
     </svg> 
   )
 }
 
 export default SimpleSVG;
+
+// this function calculates the points
+// https://dillieodigital.wordpress.com/2013/01/16/quick-tip-how-to-draw-a-star-with-svg-and-javascript/
+// function SimpleSVG() {
+  
+//   function CalculateStarPoints(centerX, centerY, arms, outerRadius, innerRadius)
+// {
+//    var results = "";
+
+//    var angle = Math.PI / arms;
+
+//    for (var i = 0; i < 2 * arms; i++)
+//    {
+//       // Use outer or inner radius depending on what iteration we are in.
+//       var r = (i & 1) == 0 ? outerRadius : innerRadius;
+      
+//       var currX = centerX + Math.cos(i * angle) * r;
+//       var currY = centerY + Math.sin(i * angle) * r;
+
+//       // Our first time we simply append the coordinates, subsequet times
+//       // we append a ", " to distinguish each coordinate pair.
+//       if (i == 0)
+//       {
+//          results = currX + "," + currY;
+//       }
+//       else
+//       {
+//          results += ", " + currX + "," + currY;
+//       }
+//    }
+
+//    return results;
+// }
+
+// let answer = CalculateStarPoints(80,80,5,20,10);
+
+//   return (
+//     <div>{answer}</div>
+//   )
+// }
+
+// export default SimpleSVG;
